cba55cb2b6a0823d3b2c744f0f8e473c5a811f15
Jennifer-Barahona <est.jennifer.barah@unimilitar.edu.co>
2026-04-24 09:39:25 -0500

    Conectamos el js con el html y agregamos la documentación del proyecto

---
+++ script.js
@@ -0,0 +1,7 @@
+
+/**
+ * Universidad UMNG - Facultad de Ingeniería Multimedia
+ * Asignatura: Introducción a la Computación Gráfica
+ * * Estudiante: Jennifer Barahona Suarez
+ * * Tarea: Implementar los algoritmos de rasterización manual.
+ */
